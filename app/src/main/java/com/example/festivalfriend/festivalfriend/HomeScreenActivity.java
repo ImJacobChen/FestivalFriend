c0ae2fe7c8ad51dd0fe4dc9710c6a96aec3cb9d4
JacobChen1994 <jacobsenpai@outlook.com>
2016-03-08 19:20:27 +0000

    Update to intergrate firebase

--- app/src/main/java/com/example/festivalfriend/festivalfriend/HomeScreenActivity.java
+++ app/src/main/java/com/example/festivalfriend/festivalfriend/HomeScreenActivity.java
@@ -9,6 +9,7 @@ public class HomeScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
 
+
+    }
 }
