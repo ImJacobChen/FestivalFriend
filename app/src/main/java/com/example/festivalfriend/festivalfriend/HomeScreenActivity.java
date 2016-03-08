e42584d2a678c6cb596222b4bdfc21a6791b15d8
JacobChen1994 <jacobsenpai@outlook.com>
2016-03-08 16:09:04 +0000

    Changed names from 'LogIn' to 'HomeScreen' and changed HomeScreen Layout

---
+++ app/src/main/java/com/example/festivalfriend/festivalfriend/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.festivalfriend.festivalfriend;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+}
